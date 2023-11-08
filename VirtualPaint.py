e3f429c61010148b16b32ba0235af43836234c54
devshah <devshah0404@gmail.com>
2023-11-08 21:46:24 +0530

    changes

--- VirtualPaint.py
+++ VirtualPaint.py
@@ -26,7 +26,7 @@ def getContours(img):
     for cnt in contours:
         area = cv2.contourArea(cnt)
         if(area>500):
-            # cv2.drawContours(imgResult,cnt,-1,(255,0,0) ,3)
+            cv2.drawContours(imgResult,cnt,-1,(255,0,0) ,3)
             peri = cv2.arcLength(cnt,True)
             approx = cv2.approxPolyDP(cnt,0.02*peri,True)            
             x,y,w,h = cv2.boundingRect(approx)
@@ -35,21 +35,23 @@ def getContours(img):
 def findColor(img,Colors):
     imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
     newPoints = []
-    for color,i in Colors:
+    count = 0
+    for color in Colors:
         lower = np.array([Colors[0][:3]])
         upper = np.array([Colors[0][3:]])
         mask = cv2.inRange(imgHSV,lower,upper)
         x,y = getContours(mask)
         if x!=0 and y!=0 :
-            newPoints.append([x,y,i])
-        cv2.circle(imgResult,(x,y),10,ColorValues[i],cv2.FILLED)
-        # cv2.imshow(str(color[0]),mask)
+            newPoints.append([x,y,count])
+        cv2.circle(imgResult,(x,y),10,ColorValues[count],cv2.FILLED)
+        count = count+1
+        cv2.imshow(str(color[0]),mask)
     return newPoints
 
 
 def drawOnCanvas(Points,ColorValues):
     for point in Points:
-        cv2.circle(imgResult , (point[x],point[y],10,ColorValues[point[2]],cv2.FILLED) )
+        cv2.circle(imgResult , (point[0],point[1]),10,ColorValues[point[2]],cv2.FILLED) 
     
     
     
